bd88fd55d08ddeeac39dbe2a877387cfb99a7f8d
jdelmolino <j_dmm@yahoo.es>
2016-10-19 19:36:02 +0200

    Add files via upload

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php
+return [
+	'connections' => [
+		'mongodb' => [
+			'driver' => 'mongodb',
+			'host' => 'localhost',
+			'port' => 27017,
+			'username' => 'learnlockuser',
+			'password' => 'learnlockpwd',
+			'database' => 'learninglocker'
+		],
+	]
+];
+?>
+
